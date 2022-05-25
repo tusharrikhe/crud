d6262cfb1defd2ab474bc3271d3e7b8c484d5326
tusharrikhe <48618647+tusharrikhe@users.noreply.github.com>
2022-05-25 14:31:32 +0530

    Video05-CRUD

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import './'
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome to React-Redux Contact book</h1>  
+    </div>
+  );
+}
+
+export default App;
